6bdeb6a5c8dfe19196aa438f3d8028bd0bee9ac4
witzkvn <witz.kevin05@gmail.com>
2022-09-22 16:18:37 +0200

    added update full wilder object with grades and renaming wrong skills to correct grades

--- src/controller/wilder.ts
+++ src/controller/wilder.ts
@@ -4,11 +4,13 @@ import dataSource from "../utils";
 import { IController } from "../interfaces/IController";
 import { Grade } from "../entity/Grade";
 import { IGrade } from "../interfaces/IGrade";
+import ISkillWithGrade from "../interfaces/ISkillWithGrade";
+import Skill from "../entity/Skill";
 
 const wilderController: IController = {
   create: async (req, res) => {
     try {
-      const { name, city, description, skills } = req.body;
+      const { name, city, description, grades } = req.body;
 
       const createdWilder = await dataSource.getRepository(Wilder).save({
         name,
@@ -17,12 +19,12 @@ const wilderController: IController = {
       });
 
       let isNotFoundAndBreak = false;
-      if (skills.length > 0) {
-        for (const skill of req.body.skills) {
+      if (grades.length > 0) {
+        for (const grade of req.body.grades) {
           const addRes = await createGradeForWilder(
             createdWilder.id,
-            skill.id,
-            skill.grades
+            grade.skillId,
+            grade.grades
           );
 
           if (addRes === null) {
@@ -59,10 +61,11 @@ const wilderController: IController = {
       });
 
       const formattedResponse = allWilders.map((el) => {
-        const elSkills = el.grades.map((grade) => {
+        const elGrades = el.grades.map((grade) => {
           return {
             id: grade.id,
             name: grade.skill.name,
+            skillId: grade.skill.id,
             grades: grade.grade,
           };
         });
@@ -72,7 +75,7 @@ const wilderController: IController = {
           name: el.name,
           description: el.description,
           city: el.city,
-          skills: elSkills,
+          grades: elGrades,
         };
       });
 
@@ -99,7 +102,7 @@ const wilderController: IController = {
 
       const wilderToUpdate = await dataSource
         .getRepository(Wilder)
-        .findBy({ id: wilderId });
+        .findOneBy({ id: wilderId });
       if (wilderToUpdate === null || wilderToUpdate === undefined) {
         res.status(404).send({
           message: "No wilder found with that ID.",
@@ -107,7 +110,7 @@ const wilderController: IController = {
         return;
       }
 
-      const updateRes = await dataSource.getRepository(Wilder).update(
+      await dataSource.getRepository(Wilder).update(
         { id: wilderId },
         {
           name: req.body.name,
@@ -116,7 +119,11 @@ const wilderController: IController = {
         }
       );
 
-      const wilderSkillsInDb = await dataSource.getRepository(Grade).find({
+      if (typeof req.body.grades === "undefined") {
+        req.body.grades = [];
+      }
+
+      const wilderGradesInDb = await dataSource.getRepository(Grade).find({
         where: {
           wilder: {
             id: wilderId,
@@ -127,47 +134,66 @@ const wilderController: IController = {
         },
       });
 
-      // delete grades not present in req.body.skills
-      const bodySkillsIds = req.body.skills.map((el) => el.id);
-      const gradeIdsToDelete = wilderSkillsInDb.filter((el) => {
-        if (req.body.skills.includes(el.id)) return false;
-        return true;
-      });
+      // delete grades not present in req.body.grades
+      const bodyGradesIds: number[] = req.body.grades.map(
+        (el: IGrade) => el.id
+      );
+      const wilderGradesIds: number[] = wilderGradesInDb.map((el) => el.id);
+      const gradeIdsToDelete: number[] = wilderGradesIds.filter(
+        (id) => !bodyGradesIds.includes(id)
+      );
+
       gradeIdsToDelete.forEach(async (grade) => {
-        await dataSource.getRepository(Grade).delete(grade.id);
+        await dataSource.getRepository(Grade).delete(grade);
       });
 
-      // update grades in common based on ID
-      const gradesIdsToUpdate = wilderSkillsInDb.filter((el) => {
-        return req.body.skills.includes(el.id);
+      await Promise.all(
+        req.body.grades.map(async (gradeToAdd: ISkillWithGrade) => {
+          // get the existing grades for the wilder
+          const existingGrade = await dataSource.getRepository(Grade).findOne({
+            where: {
+              skill: {
+                id: gradeToAdd.skillId,
+              },
+              wilder: {
+                id: wilderToUpdate.id,
+              },
+            },
+          });
+
+          // verify if skill exists in DB
+          const newSkill = await dataSource
+            .getRepository(Skill)
+            .findOneBy({ id: gradeToAdd.skillId });
+
+          if (newSkill === null) {
+            res.status(500).send({
+              message:
+                "Error while updating the requested wilder : skill not existing",
+            });
+          } else {
+            if (existingGrade !== null) {
+              // grade already exists : UPDATE
+              existingGrade.grade = gradeToAdd.grades;
+              existingGrade.skill = newSkill;
+              await dataSource.getRepository(Grade).save(existingGrade);
+            } else {
+              // grade does not exists : CREATE
+              const newGrade = new Grade();
+              newGrade.grade = gradeToAdd.grades;
+              newGrade.skill = newSkill;
+              newGrade.wilder = wilderToUpdate;
+              await dataSource.getRepository(Grade).save(newGrade);
+            }
+          }
+        })
+      );
+
+      res.status(204).send({
+        message: "Wilder updated successfully",
       });
-      gradesIdsToUpdate.forEach(async (grade) => {
-        await dataSource.getRepository(Grade).update(grade.id, {
-          wilder: grade.wilder,
-          grade: grade.grade,
-          skill: grade.skill,
-        });
-      });
-
-      // create grades that is not already in db
-
-      // req.body.skills.forEach(async (grade: IGrade) => {
-      //     const existingGrade = await dataSource
-      //         .getRepository(Grade)
-      //         .findBy({ id: grade.id });
-      // });
-
-      if (updateRes.affected === 0) {
-        res.status(400).send({
-          message:
-            "No update could be effectued. Please check the wilder id and your request body.",
-        });
-      } else {
-        res.status(204).send({
-          message: "Wilder updated successfully",
-        });
-      }
     } catch (error) {
+      console.log(error);
       res.status(500).send({
         message: "Error while updating the requested wilder",
       });
